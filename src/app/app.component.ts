d3c768d77ea0c46da5b652c3b589dbe3d968564e
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-01-30 07:58:16 -0500

    change componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatCardModule} from '@angular/material/card';
+import { LayoutComponent } from './shared/layout/layout.component';
+
+
+
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  MatCardModule, MatDividerModule, MatButtonModule, MatProgressBarModule],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
