ebb9df5312b89265daeb7ed8f7427735cc5a6219
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-02-14 14:04:28 -0500

    add: food list agregado

---
+++ src/app/recipes/food-list/food-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../shared/food.service';
+import { FoodComponent } from '../food/food.component';
+import { Food } from '../shared/food.model';
+
+
+@Component({
+  selector: 'app-food-list',
+  standalone: true,
+  imports: [FoodComponent],
+  templateUrl: './food-list.component.html',
+  styleUrl: './food-list.component.scss'
+})
+
+
+export class FoodListComponent implements OnInit {
+  data:Food[]= [];
+   constructor(public serviceFood:FoodService){
+ 
+   }
+ 
+   ngOnInit(): void{
+     this.data = this.serviceFood.getAllFoods();
+     console.log(this.data)
+ 
+   }
+ }
