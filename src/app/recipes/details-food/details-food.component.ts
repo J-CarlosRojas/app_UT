28ced61dfd1d3234e8c34095f6a9cb953fe83607
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-03-04 13:12:14 -0500

    add: Details food component

---
+++ src/app/recipes/details-food/details-food.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Food } from '../shared/food.model';
+import { FoodService } from '../shared/food.service';
+import { CurrencyPipe } from '@angular/common';
+
+
+
+@Component({
+  selector: 'app-details-food',
+  standalone: true,
+  imports: [RouterModule, CurrencyPipe],
+  templateUrl: './details-food.component.html',
+  styleUrl: './details-food.component.scss'
+})
+
+
+export class DetailsFoodComponent implements OnInit{
+
+  constructor(public foodService:FoodService){
+
+  }
+
+
+
+  route: ActivatedRoute = inject(ActivatedRoute)
+
+  foodId : number = -1
+  food?:Food = {
+    id:0,
+    name:'',
+    description:'',
+    category:'',
+    image:'',
+    price:0
+
+  }
+
+  ngOnInit():void{
+
+    this.foodId = Number(this.route.snapshot.params['id'])
+    
+    this.food = this.foodService.getOne(this.foodId)
+    
+  }
+
+}
