9a44fea611ea2369568bc1be51edb0ec6b820439
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-03-04 13:12:40 -0500

    update: routing

--- src/app/recipes/recipes.routes.ts
+++ src/app/recipes/recipes.routes.ts
@@ -2,21 +2,42 @@ import { Route } from "@angular/router";
 import { FoodListComponent } from "./food-list/food-list.component";
 import { HomeComponent } from "../home/home.component";
 import { FormFoodComponent } from "./form-food/form-food.component";
+import { DetailsFoodComponent } from "./details-food/details-food.component";
+import { Component } from "@angular/core";
+
 
 export default [
     {
         path:'',
-        component: HomeComponent
+        component: HomeComponent,
+        title:'Home'
     },
 
     {
         path: 'food-list',
-        component: FoodListComponent
+        component: FoodListComponent,
+        title: 'Lista de Comida'
     },
 
     {
         path: 'food-form',
-        component: FormFoodComponent
+        component: FormFoodComponent,
+        title: 'Agregar comidas'
+    },
+
+    {
+        path: 'details-food/:id',
+        component: DetailsFoodComponent,
+        title: 'Detalles de Comida'
+    },
+
+    {
+        path: 'food-form/:id',
+        component: FormFoodComponent,
+        title: 'Actualizar Comida'
     }
+
+
+   
 
 ] satisfies Route[];    
