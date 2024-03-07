247f9071f540c526476a8de0a3abfa087a80d988
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-03-07 07:59:31 -0500

    change: sidenav resposive

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,45 +1,55 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
-import {MatListModule} from '@angular/material/list';
-
-
+import { MatListModule } from '@angular/material/list';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [MatIconModule , MatButtonModule, MatToolbarModule, MatSidenavModule, RouterModule, MatListModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    RouterModule,
+    MatListModule,
+  ],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnDestroy {
+  opened = true;
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
 
-  opened = true
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
-
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
   public links = [
-
     {
       icon: 'restaurant_menu',
       label: 'Comidas',
-      routerlink: 'food/food-list'
+      routerlink: 'food/food-list',
     },
     {
       icon: 'lunch_dining',
       label: 'Agregar comidas',
-      routerlink: 'food/food-form'
-    }
-    
-
-
+      routerlink: 'food/food-form',
+    },
   ];
 
-  public open(){
-    this.opened = !this.opened
+  public open() {
+    this.opened = !this.opened;
   }
-
 }
